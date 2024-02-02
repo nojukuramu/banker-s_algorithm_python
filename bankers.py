689cea7f75273db56bdee849126b8d333be53edd
nojukuramu <119827430+nojukuramu@users.noreply.github.com>
2024-02-02 20:53:15 +0800

    Update bankers.py
    Added Safe Sequence Print
    Corrected some mistake in displaying the Deadlock Anouncer

--- bankers.py
+++ bankers.py
@@ -1,6 +1,7 @@
 jobs = {}
 total_resources = int(input("Enter total resources: "))
 numbers_of_jobs = int(input("Enter Number Of Jobs: "))
+sequence = []
     
 for i in range(numbers_of_jobs):
     name = input("Enter Job Name: ").upper()
@@ -17,6 +18,7 @@ available = total_resources - total_of_allocations
 
 rtrn= 0
 i = 0
+deadlock = False
 while jobs:
     for job_name, values in jobs.items():
         if available < values[2]:
@@ -30,8 +32,13 @@ while jobs:
 
             print("{:>2}exit {:<3} {:<3} {:<3}".format(job_name, "", rtrn, available))
             del jobs[job_name]
+            sequence.append(job_name)
             break
     i = i + 1
-    if i >= numbers_of_jobs:
+    if i > numbers_of_jobs:
+        deadlock = True
         print("There's a Deadlock")
         break
+        
+if deadlock == False:
+    print(f"Safe Sequence: {sequence}")
