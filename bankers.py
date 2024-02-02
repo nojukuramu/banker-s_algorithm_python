0130b597771840b795fb3394da6c920e08e5ee36
nojukuramu <119827430+nojukuramu@users.noreply.github.com>
2024-02-02 10:27:08 +0800

    Update bankers.py
    Optimized Printing the chart

--- bankers.py
+++ bankers.py
@@ -3,7 +3,7 @@ total_resources = int(input("Enter total resources: "))
 numbers_of_jobs = int(input("Enter Number Of Jobs: "))
     
 for i in range(numbers_of_jobs):
-    name = input("Enter Job Name: ")
+    name = input("Enter Job Name: ").upper()
     allocation = int(input("Enter Job Allocation: "))
     max_allocation = int(input("Enter Job Max: "))
     job_need = max_allocation - allocation
@@ -14,18 +14,17 @@ for job_name, values in jobs.items():
     total_of_allocations += values[0]
 
 available = total_resources - total_of_allocations
-rtrn = 0
 
+rtrn= 0
 while available < total_resources:
     for job_name, values in jobs.items():
         if available < values[2]:
             pass
         else:
             available -= values[2]
-            print("{:<2} Enter {:<10} {:<10} {:<10}".format(job_name, values[2], rtrn, available))
+            print("{:>2}     {:<3} {:<3} {:<3}".format(job_name, values[2], "", available))
 
-            # Return
             rtrn = values[1]
             available += values[1]
 
-            print("{:<2} Exit  {:<10} {:<10} {:<10}".format(job_name, values[2], rtrn, available))
+            print("{:>2}exit {:<3} {:<3} {:<3}".format(job_name, "", rtrn, available))
